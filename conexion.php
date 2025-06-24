d3b2c31f769cd1a35fba29f4193cd3cdbef347f6
DanielUDGPlus <alexis.moran0234@alumnos.udg.mx>
2025-06-23 19:25:44 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,13 +1,14 @@
 <?php
-$host = "localhost";      // O el hostname de Railway/Render
-$port = "5432";           // Puerto de PostgreSQL
-$dbname = "Panchocritic"; // Nombre de tu base de datos
-$user = "postgres";     // Cambia por el usuario real
-$password = "6754"; // Cambia por la contraseña real
+$host = "dpg-d1cvrgp5pdvs73c7k03g-a"; // host de Render
+$port = "5432";                       // Puerto por defecto de PostgreSQL
+$dbname = "panchocriticdb";            // Nombre de la base que creaste en Render
+$user = "pancho";               // Usuario generado por Render
+$password = "8lkKmOj5hG40YcOkj4jXYOha32z0B4nR";       // Contraseña generada por Render
 
 try {
     $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Conexión exitosa";
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
     die();
